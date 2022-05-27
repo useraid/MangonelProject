5d92f733ffdf5febf8d16996cbb72c6558af2179
Aryan <93074700+useraid@users.noreply.github.com>
2022-05-27 10:57:27 +0530

    Added Serial Monitor code

---
+++ SerialIR.ino
@@ -0,0 +1,11 @@
+int pin = 13;
+unsigned long duration;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+}
+void loop(){
+  duration=pulseIn(pin,HIGH);
+  Serial.println(duration);
+}
